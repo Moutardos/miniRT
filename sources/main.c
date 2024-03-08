98e6648ef9f0990836cf013c59efed93e8543dda
lcozdenm <loic.cozdenmat@gmail.com>
2024-03-08 11:53:31 +0100

    [ADD] Temporary debug main

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: lcozdenm <lcozdenm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 19:12:57 by ekhaled           #+#    #+#             */
-/*   Updated: 2024/03/07 00:18:43 by lcozdenm         ###   ########.fr       */
+/*   Updated: 2024/03/08 11:46:19 by lcozdenm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,12 +38,41 @@ static void debug_data(t_data data)
 	debug_print_point(data.settings.camera.point);
 	debug_print_vector(data.settings.camera.vector);
 	printf("%c\n", data.settings.camera.fov);
-	printf("\n");
 	printf("L : ");
 	debug_print_point(data.settings.light.point);
 	printf("%lf ", data.settings.light.ratio);
 	debug_print_rgb(data.settings.light.color);
 	printf("\n");
+	printf("\n");
+	for (int i = 0; i < data.object_array.len; i++)
+	{
+		t_object object = data.object_array.array[i];
+		if (object.type == CY)
+		{
+			printf("CY : ");
+			debug_print_point(object.cylinder.center);
+			printf("%lf ", object.cylinder.diameter);
+			printf("%lf ", object.cylinder.height);
+			debug_print_rgb(object.color);
+			printf("\n");
+		}
+		if (object.type == PL)
+		{
+			printf("PL : ");
+			debug_print_point(object.plane.point);
+			debug_print_vector(object.plane.vector);
+			debug_print_rgb(object.color);
+			printf("\n");
+		}
+		if (object.type == SP)
+		{
+			printf("SP : ");
+			debug_print_point(object.sphere.center);
+			printf("%lf ", object.sphere.diameter);
+			debug_print_rgb(object.color);
+			printf("\n");
+		}
+	}
 }
 
 int	main(int argc, char **argv)
@@ -54,6 +83,6 @@ int	main(int argc, char **argv)
 		return (1);
 	if (init_data(&data, argv[1]))
 		return (1);
-	// destroy_data(&data);
 	debug_data(data);
+	destroy_data(&data);
 }
