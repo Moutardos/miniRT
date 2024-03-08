b25f2e6f6f55c807bafcdf244f51a05272f63d6e
lcozdenm <loic.cozdenmat@gmail.com>
2024-03-08 12:35:34 +0100

    [FIX] normalized vector check not working properly..

--- sources/objects/is_vector_normalized.c
+++ sources/objects/is_vector_normalized.c
@@ -6,13 +6,15 @@
 /*   By: lcozdenm <lcozdenm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 23:50:18 by lcozdenm          #+#    #+#             */
-/*   Updated: 2024/03/08 12:07:08 by lcozdenm         ###   ########.fr       */
+/*   Updated: 2024/03/08 12:33:50 by lcozdenm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <math.h>
 
 #include "minirt.h"
 
 bool	is_vector_normalized(t_vector v)
 {
-	return (1.0 - get_magnitude(v) <= 0.0001);
+	return (fabs(1.0 - get_magnitude(v)) <= 0.0001);
 }
