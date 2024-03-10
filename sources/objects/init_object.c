31a54f163b72d5bb6efb504e0a5a27f016ac79ca
lcozdenm <loic.cozdenmat@gmail.com>
2024-03-10 17:41:42 +0100

    [FIX] Wrong return values for init objects..

--- sources/objects/init_object.c
+++ sources/objects/init_object.c
@@ -6,7 +6,7 @@
 /*   By: lcozdenm <lcozdenm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 11:06:22 by lcozdenm          #+#    #+#             */
-/*   Updated: 2024/03/08 11:37:37 by lcozdenm         ###   ########.fr       */
+/*   Updated: 2024/03/10 17:40:16 by lcozdenm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,16 @@ int	init_sphere(t_object *object, char *line)
 	line += 2;
 	ignore_space(&line);
 	if (extract_point(&object->sphere.center, &line))
-		return (2);
+		return (1);
 	ignore_space(&line);
 	if (extract_double(&object->sphere.diameter, &line))
-		return (2);
+		return (1);
 	ignore_space(&line);
 	if (extract_color(&object->color, &line))
-		return (2);
+		return (1);
 	ignore_space(&line);
 	if (*line != '\n' && *line != '\0')
-		return (2);
+		return (1);
 	return (0);
 }
 
@@ -37,18 +37,18 @@ int	init_plane(t_object *object, char *line)
 	line += 2;
 	ignore_space(&line);
 	if (extract_point(&object->plane.point, &line))
-		return (2);
+		return (1);
 	ignore_space(&line);
 	if (extract_vector(&object->plane.vector, &line))
-		return (2);
+		return (1);
 	if (!is_vector_normalized(object->plane.vector))
-		return (2);
+		return (1);
 	ignore_space(&line);
 	if (extract_color(&object->color, &line))
-		return (2);
+		return (1);
 	ignore_space(&line);
 	if (*line != '\n' && *line != '\0')
-		return (2);
+		return (1);
 	return (0);
 }
 
@@ -58,23 +58,23 @@ int	init_cylinder(t_object *object, char *line)
 	line += 2;
 	ignore_space(&line);
 	if (extract_point(&object->cylinder.center, &line))
-		return (2);
+		return (1);
 	ignore_space(&line);
 	if (extract_vector(&object->cylinder.vector, &line))
-		return (2);
+		return (1);
 	if (!is_vector_normalized(object->cylinder.vector))
-		return (2);
+		return (1);
 	ignore_space(&line);
 	if (extract_double(&object->cylinder.diameter, &line))
-		return (2);
+		return (1);
 	ignore_space(&line);
 	if (extract_double(&object->cylinder.height, &line))
-		return (2);
+		return (1);
 	ignore_space(&line);
 	if (extract_color(&object->color, &line))
-		return (2);
+		return (1);
 	ignore_space(&line);
 	if (*line != '\n' && *line != '\0')
-		return (2);
+		return (1);
 	return (0);
 }
