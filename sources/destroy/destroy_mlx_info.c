be294550782c6c85a6f110e0258e537ae7fe8d38
lcozdenm <loic.cozdenmat@gmail.com>
2024-03-10 16:20:17 +0100

    [ADD] init_img

--- sources/destroy/destroy_mlx_info.c
+++ sources/destroy/destroy_mlx_info.c
@@ -6,7 +6,7 @@
 /*   By: lcozdenm <lcozdenm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 19:47:29 by lcozdenm          #+#    #+#             */
-/*   Updated: 2024/03/08 11:49:57 by lcozdenm         ###   ########.fr       */
+/*   Updated: 2024/03/10 16:17:49 by lcozdenm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,7 @@
 
 void	destroy_mlx_info(t_mlx_info *mlx_info)
 {
-	if (mlx_info->img.img_ptr)
-		mlx_destroy_image(mlx_info->mlx_ptr, mlx_info->img.img_ptr);
+	mlx_destroy_image(mlx_info->mlx_ptr, mlx_info->img.img_ptr);
 	mlx_destroy_window(mlx_info->mlx_ptr, mlx_info->win_ptr);
 	mlx_destroy_display(mlx_info->mlx_ptr);
 	free(mlx_info->mlx_ptr);
--- sources/init/init_mlx_info.c
+++ sources/init/init_mlx_info.c
@@ -6,13 +6,28 @@
 /*   By: lcozdenm <lcozdenm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 19:29:28 by lcozdenm          #+#    #+#             */
-/*   Updated: 2024/03/08 11:56:33 by lcozdenm         ###   ########.fr       */
+/*   Updated: 2024/03/10 16:17:30 by lcozdenm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx.h"
 
 #include "minirt.h"
+
+static int	init_img(t_img *img, void *mlx_ptr)
+{
+	img->img_ptr = mlx_new_image(mlx_ptr, WIN_W, WIN_H);
+	if (!img->img_ptr)
+		return (1);
+	img->addr = mlx_get_data_addr(img->img_ptr, &img->bpp, &img->size_line,
+			&img->endian);
+	if (!img->addr)
+	{
+		mlx_destroy_image(mlx_ptr, img->img_ptr);
+		return (1);
+	}
+	return (0);
+}
 
 int	init_mlx_info(t_mlx_info *mlx_info)
 {
@@ -23,8 +38,15 @@ int	init_mlx_info(t_mlx_info *mlx_info)
 	if (!mlx_info->win_ptr)
 	{
 		mlx_destroy_display(mlx_info->mlx_ptr);
+		free(mlx_info->mlx_ptr);
 		return (1);
 	}
-	mlx_info->img.img_ptr = NULL;
+	if (init_img(&mlx_info->img, mlx_info->mlx_ptr))
+	{
+		mlx_destroy_window(mlx_info->mlx_ptr, mlx_info->win_ptr);
+		mlx_destroy_display(mlx_info->mlx_ptr);
+		free(mlx_info->mlx_ptr);
+		return (1);
+	}
 	return (0);
 }
