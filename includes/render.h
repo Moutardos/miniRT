fe105b5d4fd2b8442d0f9441c3f701bf9d89b52a
ekhaled <ekhaled@student.42.fr>
2024-03-15 03:00:43 +0100

    [ADD] Scalar product calculation util

--- includes/render.h
+++ includes/render.h
@@ -6,7 +6,7 @@
 /*   By: ekhaled <ekhaled@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 01:10:29 by ekhaled           #+#    #+#             */
-/*   Updated: 2024/03/11 17:44:51 by ekhaled          ###   ########.fr       */
+/*   Updated: 2024/03/15 03:00:12 by ekhaled          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,13 @@ void		run_rt(t_data *data);
 void		find_pix_color(unsigned int i, unsigned int j, t_data *data);
 
 t_vector	compute_ray(unsigned int i, unsigned int j, t_camera *camera);
-
+bool		is_ray_intersecting_obj(t_object *object);
 
 t_vector	create_vector(t_point start_point, t_point end_point);
 double		get_vector_magnitude(t_vector v);
-t_vector	normalize_vector(t_vector vector);
-t_vector	sum_vectors(t_vector vector1, t_vector vector2);
 bool		is_unit_vector(t_vector v);
+t_vector	normalize_vector(t_vector vector);
+double		perform_scalar_product(t_vector vector1, t_vector vector2);
+t_vector	sum_vectors(t_vector vector1, t_vector vector2);
 
 #endif
