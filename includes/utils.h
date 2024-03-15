a42f2d4a7b47f8cc7c2a321acb4863e1a65990f7
ekhaled <ekhaled@student.42.fr>
2024-03-15 02:48:35 +0100

    [ADD] Quadratic equation solving util

--- includes/utils.h
+++ includes/utils.h
@@ -6,7 +6,7 @@
 /*   By: ekhaled <ekhaled@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 14:43:07 by lcozdenm          #+#    #+#             */
-/*   Updated: 2024/03/11 18:43:59 by ekhaled          ###   ########.fr       */
+/*   Updated: 2024/03/15 02:47:51 by ekhaled          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,26 @@
 # include <stdbool.h>
 # include <stdlib.h>
 
-bool			are_doubles_equals(double x, double y);
-double			ft_dabs(double a);
-int				ft_isdigit(char c);
-unsigned int	ft_strlen(const char *s);
-int				ft_putstr_fd(char *s, int fd);
-bool			is_same_first_word(const char *s1, const char *s2);
-int				ft_strcmp(const char *str1, const char *str2);
+typedef struct s_quadratic_roots
+{
+	int	roots_nb;
+	union
+	{
+		double	*roots_none;
+		double	roots_one[1];
+		double	roots_two[2];
+	};
+}	t_quadratic_roots;
+
+t_quadratic_roots	solve_quadratic_equation(double a, double b, double c);
+
+bool				are_doubles_equals(double x, double y);
+double				ft_dabs(double a);
+int					ft_isdigit(char c);
+unsigned int		ft_strlen(const char *s);
+int					ft_putstr_fd(char *s, int fd);
+bool				is_same_first_word(const char *s1, const char *s2);
+int					ft_strcmp(const char *str1, const char *str2);
 
 /* get_next_line */
 
@@ -30,13 +43,13 @@ int				ft_strcmp(const char *str1, const char *str2);
 #  define BUFFER_SIZE 35
 # endif
 
-int				get_next_line(char **line_p, int fd);
-void			*ft_calloc(size_t nmemb, size_t size);
-int				ft_strchri(const char *str, char c);
-char			*ft_strcpy(char *dest, const char *src);
-char			*ft_strdup(char *str);
-char			*ft_strjoin(const char *s1, const char *s2);
-char			*ft_substrrange(char *buffer,
-					unsigned int start_index, unsigned int end_index);
+int					get_next_line(char **line_p, int fd);
+void				*ft_calloc(size_t nmemb, size_t size);
+int					ft_strchri(const char *str, char c);
+char				*ft_strcpy(char *dest, const char *src);
+char				*ft_strdup(char *str);
+char				*ft_strjoin(const char *s1, const char *s2);
+char				*ft_substrrange(char *buffer,
+						unsigned int start_index, unsigned int end_index);
 
 #endif
