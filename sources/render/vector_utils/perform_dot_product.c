43cc440859f99f43e65a3f2fa593a8932196e06d
ekhaled <giangrandeelia@gmail.com>
2024-03-20 20:26:40 +0100

    [FIX] Wrong dot product operation
    [ORG] Scalar product into dot product

--- sources/render/vector_utils/perform_dot_product.c
+++ sources/render/vector_utils/perform_dot_product.c
@@ -1,24 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   perform_scalar_product.c                           :+:      :+:    :+:   */
+/*   perform_dot_product.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ekhaled <ekhaled@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 02:57:05 by ekhaled           #+#    #+#             */
-/*   Updated: 2024/03/15 03:00:13 by ekhaled          ###   ########.fr       */
+/*   Updated: 2024/03/20 20:16:25 by ekhaled          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-double	perform_scalar_product(t_vector vector1, t_vector vector2)
+double	perform_dot_product(t_vector vector1, t_vector vector2)
 {
 	double	scalar_product;
 
 	scalar_product
-		= vector1.x + vector2.x
-		+ vector1.y + vector2.y
-		+ vector1.z + vector2.z;
+		= vector1.x * vector2.x
+		+ vector1.y * vector2.y
+		+ vector1.z * vector2.z;
 	return (scalar_product);
 }
