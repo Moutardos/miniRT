f09c1444351d58dee1f1415375c753141813be86
ekhaled <ekhaled@student.42.fr>
2024-03-21 03:42:33 +0100

    [ADD] Ray-sphere intersection equations + belonging verification

--- sources/render/find_pix_color.c
+++ sources/render/find_pix_color.c
@@ -6,7 +6,7 @@
 /*   By: ekhaled <ekhaled@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 00:35:49 by ekhaled           #+#    #+#             */
-/*   Updated: 2024/03/21 03:16:44 by ekhaled          ###   ########.fr       */
+/*   Updated: 2024/03/21 03:41:18 by ekhaled          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,18 @@
 void	find_pix_color(unsigned int i, unsigned int j, t_data *data)
 {
 	t_vector	ray;
+	int			object_index;
 
 	ray = compute_ray(i, j, &data->settings.camera, &data->frame);
+	object_index = 0;
+	while (object_index < data->object_array.len)
+	{
+		if (is_ray_intersecting_obj(
+				data->object_array.array + object_index,
+				&data->settings.camera, ray))
+		{
+			;
+		}
+		object_index++;
+	}
 }
