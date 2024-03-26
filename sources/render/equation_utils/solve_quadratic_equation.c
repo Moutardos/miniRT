a9f36300e78d05ed85787f20cb9b652fc9342aa4
ekhaled <ekhaled@student.42.fr>
2024-03-26 02:29:32 +0100

    [FIX] Forgotten name change

--- sources/render/equation_utils/solve_quadratic_equation.c
+++ sources/render/equation_utils/solve_quadratic_equation.c
@@ -6,7 +6,7 @@
 /*   By: ekhaled <ekhaled@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 00:59:03 by ekhaled           #+#    #+#             */
-/*   Updated: 2024/03/15 02:46:49 by ekhaled          ###   ########.fr       */
+/*   Updated: 2024/03/26 01:19:51 by ekhaled          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,19 @@ t_quadratic_roots	solve_quadratic_equation(double a, double b, double c)
 	discriminant = b * b - 4 * a * c;
 	if (discriminant < 0)
 	{
-		roots.roots_nb = 0;
-		roots.roots_none = NULL;
+		roots.nb = 0;
+		roots.none = NULL;
 	}
 	else if (are_doubles_equals(discriminant, 0))
 	{
-		roots.roots_nb = 1;
-		roots.roots_one[0] = -b / (2 * a);
+		roots.nb = 1;
+		roots.single[0] = -b / (2 * a);
 	}
 	else
 	{
-		roots.roots_nb = 2;
-		roots.roots_two[0] = (-b + sqrt(discriminant)) / (2 * a);
-		roots.roots_two[1] = (-b - sqrt(discriminant)) / (2 * a);
+		roots.nb = 2;
+		roots.distincts[0] = (-b + sqrt(discriminant)) / (2 * a);
+		roots.distincts[1] = (-b - sqrt(discriminant)) / (2 * a);
 	}
 	return (roots);
 }
