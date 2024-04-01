df49e20f955cea238faea9758b34843640da564a
ekhaled <ekhaled@student.42.fr>
2024-04-01 11:11:48 +0200

    [FIX] .. ft_min util typo ..

--- sources/utils/ft_min.c
+++ sources/utils/ft_min.c
@@ -6,13 +6,13 @@
 /*   By: ekhaled <ekhaled@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 10:43:23 by ekhaled           #+#    #+#             */
-/*   Updated: 2024/04/01 10:44:25 by ekhaled          ###   ########.fr       */
+/*   Updated: 2024/04/01 11:11:12 by ekhaled          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_min(int a, int b)
 {
-	if (a > b)
+	if (a < b)
 		return (a);
 	return (b);
 }
