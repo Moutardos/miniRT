10e2cf0049201be6341326739ace6fdf09e7c4a4
ekhaled <ekhaled@student.42.fr>
2024-04-04 06:55:34 +0200

    [RM] Useless parts in normalize_vector util already performed by other util

--- sources/render/vector_utils/normalize_vector.c
+++ sources/render/vector_utils/normalize_vector.c
@@ -6,7 +6,7 @@
 /*   By: ekhaled <ekhaled@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 13:24:36 by ekhaled           #+#    #+#             */
-/*   Updated: 2024/03/11 15:53:02 by ekhaled          ###   ########.fr       */
+/*   Updated: 2024/04/03 14:52:44 by ekhaled          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,9 @@
 t_vector	normalize_vector(t_vector vector)
 {
 	double		vector_magnitude;
-	t_vector	normalized_vector;
 
 	vector_magnitude = get_vector_magnitude(vector);
 	if (vector_magnitude == 0)
 		return (vector);
-	normalized_vector = (t_vector){
-		(1 / vector_magnitude) * vector.x,
-		(1 / vector_magnitude) * vector.y,
-		(1 / vector_magnitude) * vector.z
-	};
-	return (normalized_vector);
+	return (multiply_vector(1 / vector_magnitude, vector));
 }
