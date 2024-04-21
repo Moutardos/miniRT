1df55eea340bd3595c52129db1646951bf2e509c
lcozdenm <loic.cozdenmat@gmail.com>
2024-04-21 16:02:42 +0200

    [FIX] Color intensity for lighting not treated as double

--- sources/textures/get_colored_intensity.c
+++ sources/textures/get_colored_intensity.c
@@ -6,7 +6,7 @@
 /*   By: lcozdenm <lcozdenm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 13:04:53 by lcozdenm          #+#    #+#             */
-/*   Updated: 2024/04/12 16:46:21 by lcozdenm         ###   ########.fr       */
+/*   Updated: 2024/04/21 16:01:19 by lcozdenm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	get_colored_intensity(t_color_intensity color_intensity, 
 				t_color light_color, double intensity)
 {
-	color_intensity[0] += light_color.r / 255 * intensity;
-	color_intensity[1] += light_color.g / 255 * intensity;
-	color_intensity[2] += light_color.b / 255 * intensity;
+	color_intensity[0] += light_color.r / 255.0 * intensity;
+	color_intensity[1] += light_color.g / 255.0 * intensity;
+	color_intensity[2] += light_color.b / 255.0 * intensity;
 }
