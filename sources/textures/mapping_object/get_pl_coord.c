dd02865d380380d02dda03533fb5811a9f8bd341
ekhaled <giangrandeelia@gmail.com>
2024-04-22 02:45:52 +0200

    [CLEAN] Unnormed part

--- sources/textures/mapping_object/get_pl_coord.c
+++ sources/textures/mapping_object/get_pl_coord.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_pl_coord.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lcozdenm <lcozdenm@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ekhaled <ekhaled@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 17:50:27 by lcozdenm          #+#    #+#             */
-/*   Updated: 2024/04/22 02:27:57 by lcozdenm         ###   ########.fr       */
+/*   Updated: 2024/04/22 02:41:54 by ekhaled          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ t_texture_coordinates	get_pl_coord(t_plane *plane, t_point_info *point_info)
 	double		y;
 	double		x;
 
-	projected_position 
+	projected_position
 		= rotate_vector_upside(plane->vector, point_info->point, plane->point);
 	x = fmod(projected_position.x, 1);
 	y = fmod(projected_position.z, 1);
